392bc380c35eaeb19489d6b59141bf902ad87eb7
Esteban Falla <juan.falla@globant.com>
2021-12-24 10:34:26 -0500

    First microservice learning project

---
+++ api/grpcmicroservice/server/endpoints.go
@@ -0,0 +1,73 @@
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+
+	"github.com/EstebanFallaGlobant/globant-golang-bootcamp/api/grpcmicroservice/interfaces"
+	"github.com/EstebanFallaGlobant/globant-golang-bootcamp/api/grpcmicroservice/models"
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+)
+
+type Endpoints struct {
+	GetIsPalindrome endpoint.Endpoint
+	GetReverse      endpoint.Endpoint
+}
+
+func MakeEndpoints(svc interfaces.ServiceInterface, logger log.Logger, middlewares []endpoint.Middleware) Endpoints {
+	return Endpoints{
+		GetIsPalindrome: wrapEndpoint(makeGetIsPalindromeEndpoint(svc, logger), middlewares),
+		GetReverse:      wrapEndpoint(makeGetReverseEndpoint(svc, logger), middlewares),
+	}
+}
+
+func makeGetIsPalindromeEndpoint(svc interfaces.ServiceInterface, logger log.Logger) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req, ok := request.(*models.IsPalRequest)
+
+		if !ok {
+			level.Error(logger).Log("message", "invalid request")
+			return nil, errors.New("invalid request")
+		}
+
+		msg := svc.IsPal(req.Word)
+
+		return &models.IsPalResponse{
+			Message: msg,
+		}, nil
+	}
+}
+
+func makeGetReverseEndpoint(svc interfaces.ServiceInterface, logger log.Logger) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		level.Info(logger).Log("mensaje", "entra a get reverse endpoint")
+		req, ok := request.(*models.ReverseRequest)
+
+		msg, _ := json.Marshal(req)
+
+		level.Info(logger).Log("request", string(msg))
+
+		if !ok {
+			level.Error(logger).Log("message", "invalid request")
+			return nil, errors.New("invalid request")
+		}
+
+		level.Info(logger).Log("mensaje", "pasa if")
+		reverseString := svc.Reverse(req.Word)
+		level.Info(logger).Log("respuesta", reverseString)
+		result := new(models.ReverseResponse)
+		result.Word = reverseString
+		return result, nil
+	}
+}
+
+func wrapEndpoint(endpoint endpoint.Endpoint, middlewares []endpoint.Middleware) endpoint.Endpoint {
+	for _, middleware := range middlewares {
+		endpoint = middleware(endpoint)
+	}
+
+	return endpoint
+}
--- go.mod
+++ go.mod
@@ -8,7 +8,13 @@ require (
 )
 
 require (
+	github.com/go-kit/log v0.2.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-kit/kit v0.12.0
 	github.com/gorilla/mux v1.8.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.3.0 // indirect
